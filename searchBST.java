093fd40e38d517ec08c008c79c23e44304ac743b
weichen3 <weichen3@uvic.ca>
2020-10-20 23:16:45 -0700

    Q700 Search in a BST

---
+++ searchBST.java
@@ -0,0 +1,9 @@
+class searchBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+        
+        if (root.val > val) return searchBST(root.left, val);
+        return searchBST(root.right, val);    
+    }           
+}
