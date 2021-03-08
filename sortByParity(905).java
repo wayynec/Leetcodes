5f7917bdee5dc38842ab604543be8e85c05ba5eb
weichen3 <weichen3@uvic.ca>
2021-03-07 23:47:36 -0800

    905 - Sort Array By Parity

---
+++ sortByParity(905).java
@@ -0,0 +1,13 @@
+public int[] sortArrayByParity(int[] A) {
+    int i = 0, j = A.length-1;
+    while (i < j) {
+        if (A[i]%2 != 0 && A[j]%2 ==0) {
+            int temp = A[i];
+            A[i++] = A[j];
+            A[j--] = temp;
+        }
+        if (A[i]%2 == 0) i++;
+        if (A[j]%2 != 0) j--;
+    }
+    return A;
+}
