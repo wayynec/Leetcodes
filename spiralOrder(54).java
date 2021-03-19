6769685f7430ff6afbf9996949e72d57d3dc15f6
weichen3 <weichen3@uvic.ca>
2021-03-18 23:59:27 -0700

    54 - Spiral Matrix

---
+++ spiralOrder(54).java
@@ -0,0 +1,26 @@
+public List<Integer> spiralOrder(int[][] matrix) {
+    List<Integer> result = new ArrayList<>();
+    if (matrix == null || matrix.length == 0) return result;
+    int total = matrix.length * matrix[0].length;
+    int top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
+    
+    while (result.size() < total) {
+        for (int i = left; i <= right && result.size() < total; i++) {
+            result.add(matrix[top][i]);
+        }
+        top++;
+        for (int i = top; i <= bottom && result.size() < total; i++) {
+            result.add(matrix[i][right]);
+        }
+        right--;
+        for (int i = right; i >= left && result.size() < total; i--) {
+            result.add(matrix[bottom][i]);
+        }
+        bottom--;
+        for (int i = bottom; i >= top && result.size() < total; i--) {
+            result.add(matrix[i][left]);
+        }
+        left++;
+    }
+    return result;
+}
